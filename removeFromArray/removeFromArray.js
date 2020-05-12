8170034b15719fb7fe40a8f57d6b45a85a807acc
Lauren Hanano <lmhanano@gmail.com>
2020-05-12 15:44:38 -0700

    Completed with solutions

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
-}
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item)
+        }
+    })
+    return newArray
+    }
 
 module.exports = removeFromArray
+
+
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(i, j) {
+    let result = 0;
+    if (!Number.isInteger(i) || !Number.isInteger(j)) {result = 'ERROR';}
+    else if (i<0 || j<0) {result = 'ERROR';}
+    else if (i <= j) {
+        for (; i<=j; i++) {result += i;}
+    } else if (i >= j) {
+        for (; i>=j; i--) {result += i;}
+    }
+    return result;
 }
 
 module.exports = sumAll
+
