2d708f73d19db3564995b28cc2a3b97f9bcec42c
lhanano <lmhanano@gmail.com>
2020-07-28 10:22:55 -0700

    completed with solutions

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    const newNum = num % 26;
+    const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const strLowerCase = str.toLowerCase();
+    let newStr = '';
 
+    for (let i = 0; i < str.length; i++) {
+        const currentLetter = strLowerCase[i];
+        if (currentLetter === ' ' || currentLetter === '!' || currentLetter === ',') {
+            newStr += currentLetter;
+            continue;
+        }
+        const strIndex = alpha.indexOf(currentLetter);
+        let strNewIndex = strIndex + newNum;
+        if (strNewIndex > 25) strNewIndex -= 26;
+        if (strNewIndex < 0) strNewIndex += 26;
+        if (str[i] === currentLetter) newStr += alpha[strNewIndex];
+        else newStr += alpha[strNewIndex].toUpperCase();
+    }
+    return newStr;
 }
 
 module.exports = caesar
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    const processedString = string.toLowerCase().replace(/[^\w\s]|_| /g, '');
+    const reversedString = processedString.split('').reverse().join('');
+    return processedString === reversedString;
 }
 
 module.exports = palindromes
