bcc5cacb19b7eef1beea6eeab6cd8304cb0108f6
lhanano <lmhanano@gmail.com>
2020-07-28 10:15:14 -0700

    completed with solutions

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
-}
+const fibonacci = function(n) {
+    if (n<0) return 'OOPS';
+    let fib = [1, 1];
+    for (let i = fib.length; i<n; i++) {
+        fib.push(fib[fib.length-1]+fib[fib.length-2]);
+    };
+    return fib[fib.length-1];
+    }
 
 module.exports = fibonacci
